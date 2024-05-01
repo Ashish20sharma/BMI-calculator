5670d7b5f06b4f9fe814631b3d14cb49c2b1bee1
Ashish20sharma <133103644+Ashish20sharma@users.noreply.github.com>
2024-05-01 11:14:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useMemo, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [weight, setWeight] = useState(70);
+  const [height, setHeight] = useState(100);
+
+  const BMI=useMemo(()=>{
+    let heightCalculate=height/100;
+
+    return(weight/(heightCalculate*heightCalculate)).toFixed(1)
+  },[weight,height])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main" >
+      <div className='box'>
+        <h1>BMI Calculator</h1>
+        <div>
+          <p>Weight:{weight} kg</p>
+          <input type="range" onClick={(e) => setWeight(e.target.value)} name="weight" id="weight" max="200" />
+        </div>
+        <div>
+          <p>Height:{height} cm</p>
+          <input type="range" onClick={(e) => setHeight(e.target.value)} name="Height" id="Height" max="200" />
+        </div>
+        <h1>Your BMI is:{BMI}</h1>
+      </div>
     </div>
   );
 }
